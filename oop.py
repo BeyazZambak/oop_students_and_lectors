5f4e9ce8a50e40f9367ae82970e311f3a099a734
Beyaz Zambak <v1kusen04ek@mail.ru>
2024-06-24 14:26:13 +0300

    Task 2

--- oop.py
+++ oop.py
@@ -10,12 +10,27 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name) 
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            print('Error')
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-    
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -23,22 +38,24 @@ class Mentor:
             else:
                 student.grades[course] = [grade]
         else:
-            return 'Ошибка'
-
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
+            print('Error')
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+cool_lecturer = Lecturer("Best", "Lecturer")
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
+best_student.rate_lecturer(cool_lecturer, 'Python', 5)
+best_student.rate_lecturer(cool_lecturer, 'Python', 8)
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
  
-print(best_student.grades)
+print(f"Student grades ({best_student.name} {best_student.surname}): {best_student.grades}")
+print(f"Lecturer's grades ({cool_lecturer.name} {cool_lecturer.surname}): {cool_lecturer.grades}")
